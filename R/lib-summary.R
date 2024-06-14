a43cd45ca3407a22bd018ce233c156f1aec9c5e0
Tobias Fietze <tobias.fietze@slu.se>
2024-06-14 12:14:24 +0200

    Managing Dependencies

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -10,10 +10,25 @@
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
+  if (!is.logical(sizes)) {
+    stop("'sizes' must be logical (TRUE/FALSE).")
+  }
+
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if (sizes) {
+    library("fs")
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x) {
+        sum(file_size(dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1)
+    )
+  }
   pkg_df
 }
--- tests/testthat/test-tofilibminer2-package.R
+++ tests/testthat/test-tofilibminer2-package.R
@@ -7,6 +7,15 @@ test_that("lib_summary returns expected results", {
   expect_type(res$n_packages, "integer")
 })
 
+
 test_that("lib_summary fails appropriately", {
-  expect_error(lib_summary("foo"), "unused argument")
+  expect_error(lib_summary(sizes = "foo"), "not interpretable as logical")
 })
+
+
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  expect_type(res$lib_size, "double")
+})
+
